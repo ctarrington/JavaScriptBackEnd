7bc3b06aec3b2edaa2b28862d4e7758fc0b59444
ctarrington <arrington.ct@gmail.com>
2015-10-12 14:38:42 -0400

    added client

--- PerformanceTests/app.js
+++ PerformanceTests/app.js
@@ -9,13 +9,29 @@ var fs = require('fs');
 // returns the path to the word list which is separated by `\n`
 var wordListPath = require('word-list');
 
-var wordList = fs.readFileSync(wordListPath, 'utf8').split('\n');
-//=> [..., 'abmhos', 'abnegate', ...]
-
 app.use(express.static('client'));
+
+var LIMIT = 10000;
 
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
+
+function busyWait(delay)
+{
+    var start = Date.now();
+    var totalCtr = 0;
+    var done = false;
+
+    while (!done) {
+        for (var ctr=0;ctr < 100000;ctr++)
+        {
+            totalCtr++;
+        }
+        var current = Date.now();
+        var elapsed = current - start;
+        done = (elapsed >= delay);
+    }
+}
 
 function reverse(word)
 {
@@ -31,82 +47,95 @@ function reverse(word)
 }
 
 function compareLength(wm1, wm2) {
-    if (wm1.match.length > wm2.match.length) {
+    if (wm1.word.length > wm2.word.length) {
         return -1;
     }
-    if (wm1.match.length < wm2.match.length) {
+    if (wm1.word.length < wm2.word.length) {
         return 1;
     }
 
     return 0;
 }
 
+function wrapWord(word) {
+    return {word: word, length: word.length, upper: word.toUpperCase()};
+}
+
+function findLongest(wm, currentLong) {
+    return (wm.word.length > currentLong.word.length) ? wm : currentLong;
+}
+
+function filterPalindromes(wm) {
+    return (wm.word === reverse(wm.word));
+}
+
+function buildResponse(wordList, req) {
+
+    var start = Date.now();
+
+    var re = new RegExp(req.query.q, 'i');
+
+    function matcher(wrapped) {
+        return (wrapped.word.match(re));
+    }
+
+    var wrappedWords = wordList.map(wrapWord);
+
+    var matches = wrappedWords.filter(matcher);
+
+    var palindromes = matches.filter(filterPalindromes);
+    var longestPalindrome = (palindromes.length > 0) ? palindromes.reduce(findLongest) : null;
+
+    var response = {
+        query: req.query.q,
+        longest: matches.reduce(findLongest),
+        length: matches.length,
+        lost: 0,
+        longestPalindrome: longestPalindrome,
+        palindromes: palindromes,
+        matches: matches.sort(compareLength)
+    };
+
+
+    if (matches.length > LIMIT) {
+        response.lost = response.matches.length - LIMIT;
+        response.matches = response.matches.slice(0, LIMIT);
+        response.length = response.matches.length;
+    }
+
+    var elapsed = Date.now() - start;
+    console.log('elapsed='+elapsed);
+    return response;
+}
+
 var server = app.listen(3000, function () {
 
-    app.post('/createLargeFile', function(req, res) {
-
-        res.send({messge:'hi', words: wordList.slice(0, 20), length: wordList.length});
-
-    });
-
-    app.get('/readLargeFile', function(req, res) {
-
-        res.send({messge:'hi'});
-
-    });
-
-    app.get('/wordLengthHistogramForFile', function(req, res) {
-
-        res.send({messge:'hi'});
-
-    });
+    var wordList = null;
 
     app.get('/findMatches', function(req, res) {
 
-        var re = new RegExp(req.query.q, 'i');
-        var LIMIT = 10000;
-
-        function matcher(word)
+        if (!wordList)
         {
-            return (word.match(re));
+            // read 2.8M of words
+            fs.readFile(wordListPath, 'utf8', function(err, data) {
+
+                if (err) {
+                    res.status(500).send({error: err});
+                    return;
+                }
+
+                wordList = data.split('\n');
+
+                res.send(buildResponse(wordList, req));
+            });
+        }
+        else
+        {
+            res.send(buildResponse(wordList, req));
         }
 
-        var matches = wordList.filter(matcher);
-        var wrappedMatches = matches.map(function wrap(word) {
-           return {match: word, length: word.length, upper: word.toUpperCase()};
-        });
-
-        wrappedMatches = wrappedMatches.sort(compareLength)
-
-        var longest = wrappedMatches.reduce(function findLongest(wm, currentLong) {
-            return (wm.match.length > currentLong.match.length) ? wm : currentLong;
-        });
-
-        var palindromes = wrappedMatches.filter(function filterPalindromes(wm) {
-            return (wm.match === reverse(wm.match));
-        });
-
-        var response = {};
-        response.query = req.query.q;
-        response.longest = longest;
-        response.length = wrappedMatches.length;
-        response.lost = 0;
-        response.wrappedPalindromes = palindromes;
-        response.wrappedMatches = wrappedMatches;
-
-
-        if (response.wrappedMatches.length > LIMIT) {
-            response.lost = response.wrappedMatches.length - LIMIT;
-            response.wrappedMatches = response.wrappedMatches.slice(0, LIMIT);
-            response.length = response.wrappedMatches.length;
-        }
-
-
-
-        res.send(response);
 
     });
-
 
 
     console.log('Performance Tests listening at http://%s:%s', server.address().address, server.address().port);
---
+++ PerformanceTests/client.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var request = require('superagent');
+var Promise = require("bluebird");
+
+
+
+
+function makeCall(id) {
+
+    var start = Date.now();
+    var stop = null;
+
+    var promise = new Promise(function (resolve, reject) {
+        request
+            .get('http://localhost:3000/findMatches?q=^back.*[s]$')
+            .set('Accept', 'application/json')
+            .end(function(err, res){
+
+                stop = Date.now();
+
+                if (err) { reject(err); return; }
+                resolve(res);
+            });
+    });
+
+    return {
+        promise: promise,
+        elapsed: function() { return stop-start; },
+        id: id
+    };
+}
+
+function burst(size)
+{
+    var calls = [];
+
+    var startBurst = Date.now();
+
+    for (var ctr=0; ctr<size;ctr++)
+    {
+        var call = makeCall(ctr);
+        calls.push(call);
+    }
+
+    var promises = calls.map(function(call) {return call.promise; });
+    Promise.all(promises).then(function() {
+        var elapsedTimes = calls.map(function(call) {return call.elapsed();});
+        console.log('elapsedTimes = '+elapsedTimes.join(','));
+        var elapsedBurst = Date.now()-startBurst;
+        console.log('burst elapsed time = '+elapsedBurst);
+
+    });
+
+}
+
+burst(10);
