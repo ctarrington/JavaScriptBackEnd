3c56cbf90e91409c355e628001699f58135993eb
ctarrington <arrington.ct@gmail.com>
2016-03-06 18:19:12 -0500

    flow validation across requires

--- TryFlow/src/index.js
+++ TryFlow/src/index.js
@@ -1,6 +1,6 @@
 /* @flow  */
 
-var s = require('./samples.js');
+var samples = require('./samples.js');
 
 console.log('hi');
 
@@ -11,13 +11,14 @@ function add(a/*:number*/ ,b/*:number*/ )
 
 add(1, 2);
 add(1,'b');
+add('a', 'b');
 
 /**
  * Tests for one or more letters followed by one or more numbers.
  * @param  {string}  candidate specified string to be tested.
  * @return {Boolean}           true if candidate matches rule.
  */
-function hasLetterAndNumber(candidate)
+function hasLetterAndNumber(candidate/*:string*/)
 {
   var re = /[a-zA-Z]+[0-9]+/;
   return (re.test(candidate) );
@@ -27,7 +28,7 @@ hasLetterAndNumber('a1');
 hasLetterAndNumber('11');
 hasLetterAndNumber(1);
 
-var foo = (Math.random() > 0.5) ? 'hi1' : undefined;
+var foo = samples.foo();
 hasLetterAndNumber(foo);
 
 hasLetterAndNumber(40);
--- TryFlow/src/samples.js
+++ TryFlow/src/samples.js
@@ -3,11 +3,11 @@
 
   function foo()
   {
-
+    return (Math.random() > 0.5) ? 'hi1' : undefined;
   }
 
   module.exports = {
     foo: foo
   };
-  
+
 })();
